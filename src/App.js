12befb6706492639e1b63403252d90a8139d3c12
Roxolana <roxolanapavlyk@gmail.com>
2019-02-06 12:29:51 +0100

    the route file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/Form'
+import './output.css'
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main_container">
+      <div className='myBorder'></div>
+  <div className='introWords'>
+  <h1>Hello :) <span>Welcome to my weather page!!!!</span></h1>
+  </div>
+<Form></Form>
+  
       </div>
+      
     );
   }
 }
