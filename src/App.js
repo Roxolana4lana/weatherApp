1a778b53c8d30a515448abbcbd603805877985d6
Roxolana <roxolanapavlyk@gmail.com>
2019-02-27 22:52:36 +0100

    clean up the component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import Form from './components/Form'
 import './output.css'
 
 
-
 class App extends Component {
   render() {
     return (
       <div className="main_container">
-      <div className='myBorder'></div>
-  <div className='introWords'>
-  <h1>Hello :) <span>Welcome to my weather page!!!!</span></h1>
-  </div>
-<Form></Form>
-  
+        <div className='myBorder'></div>
+        <div className='introWords'>
+          <h1>Hello :) <span>Welcome to my weather page!!!!</span></h1>
+         </div>
+        <Form/>
       </div>
-      
-    );
+    )
   }
 }
 
-export default App;
+export default App
