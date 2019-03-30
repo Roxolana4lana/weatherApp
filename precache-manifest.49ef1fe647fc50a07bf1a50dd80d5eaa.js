77c97e5b23e8d3b568ead0bf90dde893dab1fb0e
Roxolana <roxolanapavlyk@gmail.com>
2019-03-30 23:06:45 +0100

    Updates

---
+++ precache-manifest.49ef1fe647fc50a07bf1a50dd80d5eaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ff3e20d12400699242a",
+    "url": "/weatherApp/static/js/runtime~main.0ff3e20d.js"
+  },
+  {
+    "revision": "9bb1f3a23b4757e9551c",
+    "url": "/weatherApp/static/js/main.9bb1f3a2.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/weatherApp/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "9bb1f3a23b4757e9551c",
+    "url": "/weatherApp/static/css/main.fb00d69d.chunk.css"
+  },
+  {
+    "revision": "c80cf832d272067c1933cd8b97ebc26f",
+    "url": "/weatherApp/index.html"
+  }
+];
