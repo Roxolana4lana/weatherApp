8720e33d762dc0310e54421273d9cc2644aa00fc
Roxolana <roxolanapavlyk@gmail.com>
2019-03-30 22:48:35 +0100

    clean up component

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -5,8 +5,8 @@ export default class WeatherData extends Component {
   render() {
     return (
       <div>
-        <h1>The temperature is {this.props.temperatura} &#8451;</h1>
-        <h2>The speed of the wind is {this.props.wind} km/hour</h2>
+        <h1>Temperature: {this.props.temperatura} &#8451;</h1>
+        <h2>Speed of the wind: {this.props.wind} km/hour</h2>
         <h2>You may describe the weather as: {this.props.weather}</h2> 
       </div>
     )
